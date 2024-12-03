66ef79b6353d6dcb833f8380fb9fb53b9d2a77d5
Sheetal Varsani <sheetalvarsani@hotmail.co.uk>
2024-12-03 15:24:12 +0000

    Update ReadMe File

--- ParkingLot/src/Main.java
+++ ParkingLot/src/Main.java
@@ -1,8 +1,6 @@
 import parkinglot.ParkingLot;
 import parkinglot.vehicles.*;
 
-import parkinglot.spots.*;
-
 public class Main {
     public static void main(String[] args) {
         ParkingLot parkingLot = new ParkingLot(3, 4);
