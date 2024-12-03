8437afd71ba44be54d9a786a99b441c0a420ef45
Sheetal Varsani <sheetalvarsani@hotmail.co.uk>
2024-12-03 12:23:17 +0000

    Added ParkingSpots class and ParkingLot class and updated Main for testing

--- ParkingLot/src/Main.java
+++ ParkingLot/src/Main.java
@@ -1,5 +1,22 @@
+import parkinglot.vehicles.*;
+
+import parkinglot.spots.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Vehicle motorcycle = new Motorcycle();
+        Vehicle car = new Car();
+        Vehicle van = new Van();
+
+        System.out.println("Motorcycle needs spots: " + motorcycle.getSpotsNeeded());
+        System.out.println("Car needs spots: " + car.getSpotsNeeded());
+        System.out.println("Van needs spots: " + van.getSpotsNeeded());
+
+        ParkingSpots compactSpot = new ParkingSpots(VehicleSize.COMPACT);
+
+        System.out.println("Car can park in compact spot: " + compactSpot.canFitVehicle(car));
+        System.out.println("Motorcycle can park in compact spot: " + compactSpot.canFitVehicle(motorcycle));
+        System.out.println("Van can park in compact spot: " + compactSpot.canFitVehicle(van));
+
     }
 }
